c6bbaaab2490be4823ffa5c95914037658814688
QamarQ <kamilm@you2.pl>
2025-01-22 19:47:09 +0100

    feat: add shadcn/ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,22 @@
 import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-red-500">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="flex flex-col gap-2">
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <Button>Click on the Vite and React logos to learn more</Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
